76c29b2cc7636f467c4425b7cd706132e78246bd
lopesma2 <59024271+lopesma2@users.noreply.github.com>
2022-04-04 09:34:13 -0300

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,30 @@
-#!/bin/bash -xe
-sudo curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y git vim wget gnupg lsb-release
+
+
+sudo curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
-sudo sleep 1
 
-sudo cd /home/ubuntu/teste-devops/
+
+cd /home/ubuntu/
+sudo git clone https://github.com/lopesma2/teste-devops.git
+
+cd /home/ubuntu/teste-devops/
 sudo git clone https://github.com/herbsjs/todolist-on-herbs.git
-sudo cd todolist-on-herbs/
+cd todolist-on-herbs/
 sudo npm install
+cd /home/ubuntu/teste-devops/
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo apt-get update
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo docker-compose -f docker-compose.yml up -d --build
